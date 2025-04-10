7aad66b2febb14b62d2753699991578d19d8231a
iTop1337 <lucas.adcost@gmail.com>
2025-04-10 16:50:56 -0300

    PUT

--- src/main/java/com/example/crud/domain/product/RequestProduct.java
+++ src/main/java/com/example/crud/domain/product/RequestProduct.java
@@ -2,6 +2,15 @@ package com.example.crud.domain.product;
 
 import org.antlr.v4.runtime.misc.NotNull;
 
-public record RequestProduct(String name, Integer price_in_cents) {
+public record RequestProduct(
+
+        String id,
+
+
+        String name,
+
+        @NotNull
+        Integer price_in_cents) {
+
 
 }
